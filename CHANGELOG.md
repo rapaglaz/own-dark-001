1b31c80682c07549cbee9175812f1acec514ec1f
rapaglaz <paul@rapaglaz.de>
2025-02-12 08:10:11 +0100

    chore: format codebase

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,16 +2,16 @@
 
 ### Changes
 
-* update .vscodeignore ([a89d4dd](https://github.com/rapaglaz/own-dark-001/commit/a89d4dd13e672ad97ab42b8dfed1dc6b0f6988f4))
+- update .vscodeignore ([a89d4dd](https://github.com/rapaglaz/own-dark-001/commit/a89d4dd13e672ad97ab42b8dfed1dc6b0f6988f4))
 
 ## [1.1.38](https://github.com/rapaglaz/own-dark-001/compare/1.1.37...1.1.38) (2025-02-10)
 
 ### Changes
 
-* update screenshot ([09e3be9](https://github.com/rapaglaz/own-dark-001/commit/09e3be9f8f7f12b3c12de2a74dcebaa9fd9e49ef))
+- update screenshot ([09e3be9](https://github.com/rapaglaz/own-dark-001/commit/09e3be9f8f7f12b3c12de2a74dcebaa9fd9e49ef))
 
 ## [1.1.37](https://github.com/rapaglaz/own-dark-001/compare/1.1.36...1.1.37) (2025-02-10)
 
 ### Changes
 
-* update README.md ([ad2205d](https://github.com/rapaglaz/own-dark-001/commit/ad2205dd990267f86d1545c8a0643ef851324785))
+- update README.md ([ad2205d](https://github.com/rapaglaz/own-dark-001/commit/ad2205dd990267f86d1545c8a0643ef851324785))
--- release.config.cjs
+++ release.config.cjs
@@ -2,79 +2,79 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-	branches: ['main'],
-	tagFormat: '${version}',
-	plugins: [
-		[
-			'@semantic-release/commit-analyzer',
-			{
-				preset: 'angular',
-				releaseRules: [
-					{
-						breaking: true,
-						release: 'major',
-					},
-					{
-						type: 'feat',
-						release: 'minor',
-					},
-					{
-						type: 'fix',
-						release: 'patch',
-					},
-					{
-						type: 'style',
-						release: 'patch',
-					},
-				],
-				parserOpts: {
-					noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
-				},
-			},
-		],
-		[
-			'@semantic-release/release-notes-generator',
-			{
-				preset: 'conventionalcommits',
-				presetConfig: {
-					types: [
-						{ type: 'feat', section: 'Changes', hidden: false },
-						{ type: 'fix', section: 'Changes', hidden: false },
-						{ type: 'style', section: 'Changes', hidden: false },
-					],
-				},
-				parserOpts: {
-					noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
-				},
-			},
-		],
-		['@semantic-release/npm'],
-		[
-			'@semantic-release/changelog',
-			{
-				changelogFile: 'CHANGELOG.md',
-			},
-		],
-		[
-			'@semantic-release/git',
-			{
-				assets: ['package.json', 'CHANGELOG.md'],
-				message:
-					'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
-			},
-		],
-		[
-			'semantic-release-vsce',
-			{
-				packageVsix: true,
-				publish: true,
-			},
-		],
-		[
-			'@semantic-release/github',
-			{
-				assets: '*.vsix',
-			},
-		],
-	],
+  branches: ['main'],
+  tagFormat: '${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {
+            breaking: true,
+            release: 'major',
+          },
+          {
+            type: 'feat',
+            release: 'minor',
+          },
+          {
+            type: 'fix',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+        ],
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Changes', hidden: false },
+            { type: 'fix', section: 'Changes', hidden: false },
+            { type: 'style', section: 'Changes', hidden: false },
+          ],
+        },
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+      },
+    ],
+    ['@semantic-release/npm'],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      'semantic-release-vsce',
+      {
+        packageVsix: true,
+        publish: true,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: '*.vsix',
+      },
+    ],
+  ],
 };
