49486f1ba0d938ae585106e05a47a5a31dd78ea7
rapaglaz <paul@rapaglaz.de>
2025-05-13 20:03:33 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# VS Code dark theme inspired by the Dark Modern and One Dark themes
+
+Big thanks for inspiration to authors of these great themes [One Dark Theme](https://marketplace.visualstudio.com/items?itemName=mskelton.one-dark-theme) and [One Dark Pro](https://marketplace.visualstudio.com/items?itemName=zhuangtongfa.Material-theme).
+
+See VSCode settings:
+[workbench.editor.showTabs](vscode://settings/workbench.editor.showTabs)
+
+## Screenshots
+
+![Screenshot](https://github.com/rapaglaz/own-dark-001/blob/main/screenshots/screenshot-10.png?raw=true)
+
+# VS Code dark theme inspired by the Dark Modern and One Dark themes
+
+A sleek and focused dark theme for Visual Studio Code, blending inspiration from _Dark Modern_ and _One Dark_ aesthetics.
+
+Inspired by:
+[One Dark Theme](https://marketplace.visualstudio.com/items?temName=mskelton.one-dark-theme) & [One Dark Pro](https://marketplace.visualstudio.com/items?itemName=zhuangtongfa.Material-theme)
+
+## Features
+
+- Clean, modern color palette
+- Great contrast for long coding sessions
+- Ideal for frontend and full-stack development
+
+## Customization Tip
+
+For a minimal UI, try adjusting the following setting in your VS Code config:
+
+```json
+{
+  "workbench.editor.showTabs": false
+}
+```
---
+++ releaserc.config.js
@@ -0,0 +1,125 @@
+const { publish } = require('@vscode/vsce');
+
+module.exports = {
+  $schema: 'https://semantic-release.org/schema/semantic-release.schema.json',
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          {
+            type: 'feat',
+            release: 'minor',
+          },
+          {
+            type: 'fix',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+          {
+            type: 'perf',
+            release: false,
+          },
+          {
+            type: 'docs',
+            release: false,
+          },
+          {
+            type: 'refactor',
+            release: false,
+          },
+          {
+            type: 'test',
+            release: false,
+          },
+          {
+            type: 'chore',
+            release: false,
+          },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            {
+              type: 'feat',
+              section: 'Changes',
+            },
+            {
+              type: 'fix',
+              section: 'Changes',
+            },
+            {
+              type: 'style',
+              section: 'Changes',
+            },
+            {
+              type: 'perf',
+              hidden: true,
+            },
+            {
+              type: 'docs',
+              hidden: true,
+            },
+            {
+              type: 'chore',
+              hidden: true,
+            },
+            {
+              type: 'refactor',
+              hidden: true,
+            },
+            {
+              type: 'ci',
+              hidden: true,
+            },
+            {
+              type: 'test',
+              hidden: true,
+            },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      'semantic-release-vsce',
+      {
+        packageVsix: true,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: '*.vsix',
+      },
+    ],
+  ],
+};
