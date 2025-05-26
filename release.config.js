946031a09eb741699965fe5fcd6753e3b320d946
rapaglaz <paul@rapaglaz.de>
2025-05-27 00:37:26 +0200

    chore: do not add releasedLabels to PR and Issues

--- release.config.js
+++ release.config.js
@@ -112,6 +112,7 @@ module.exports = {
         assets: '*.vsix',
         releaseNameTemplate: '<%= nextRelease.version %>',
         releaseBodyTemplate: '<%= nextRelease.notes %>',
+        releasedLabels: false,
       },
     ],
   ],
