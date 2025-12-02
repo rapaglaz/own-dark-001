dfaef5c32610a4af9d854516480819b271c7e29d
rapaglaz <paul@rapaglaz.de>
2025-12-02 22:34:58 +0100

    fix: use GITHUB_TOKEN and configure git author for semantic-release

--- release.config.js
+++ release.config.js
@@ -108,6 +108,13 @@ module.exports = {
       },
     ],
     [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
       '@semantic-release/github',
       {
         assets: '*.vsix',
